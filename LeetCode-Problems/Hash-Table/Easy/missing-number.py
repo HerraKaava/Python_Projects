911c8ff80aef08617b2c28356de47d8c43fa9259
HerraKaava <113375055+HerraKaava@users.noreply.github.com>
2023-12-28 11:24:56 +0200

    Add files via upload

---
+++ LeetCode-Problems/Hash-Table/Easy/missing-number.py
@@ -0,0 +1,35 @@
+class Solution:
+    def missingNumber(self, nums: list[int]) -> int:
+        """
+        Given an array nums containing n distinct numbers in the range [0, n],
+        return the only number in the range that is missing from the array.
+
+        Examples:
+            Input: nums = [3,0,1]
+            Output: 2
+
+            Input: nums = [0,1]
+            Output: 2
+
+            Input: nums = [9,6,4,2,3,5,7,0,1]
+            Output: 8
+
+        Notes:
+            All the numbers of nums are unique.
+        """
+        # The sum of n natural numbers 0,1,2,...,n can be calculated using the classic formula:
+        # S_n = n*(n+1)/2
+        n = len(nums)
+        expected_sum = n * (n + 1) // 2
+        observed_sum = sum(nums)
+        return expected_sum - observed_sum
+
+
+if __name__ == "__main__":
+    sol = Solution()
+    nums1 = [3, 0, 1]
+    nums2 = [0, 1]
+    nums3 = [9,6,4,2,3,5,7,0,1]
+    print(sol.missingNumber(nums1))
+    print(sol.missingNumber(nums2))
+    print(sol.missingNumber(nums3))
