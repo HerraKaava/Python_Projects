f1b4bd86a148805fb838fdfa7c876b3f8f9854f9
HerraKaava <herra.kaava@gmail.com>
2024-02-23 14:40:09 +0200

    Made the loop more efficient

--- LeetCode-Problems/Array/Easy/two-sum.py
+++ LeetCode-Problems/Array/Easy/two-sum.py
@@ -9,7 +9,7 @@ class Solution:
         """
         # This problem can be tackled e.g. using a nested loop.
         for i in range(len(nums) - 1):
-                for j in range(1, len(nums)):
+                for j in range(i+1, len(nums)):
                     if (nums[i] + nums[j] == target) and (i != j):
                         return [i, j]
 
