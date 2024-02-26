6f070cecd272a075896f26c631fd9b9559f3b7bd
HerraKaava <herra.kaava@gmail.com>
2024-02-26 16:54:06 +0200

    Made the function simpler

--- LeetCode-Problems/Sorting/Easy/valid-anagram.py
+++ LeetCode-Problems/Sorting/Easy/valid-anagram.py
@@ -6,10 +6,7 @@ class Solution:
         An Anagram is a word formed by rearranging the letters of a different word,
         typically using all the original letters exactly once.
         """
-        if sorted(s) == sorted(t):
-            return True
-        else:
-            return False
+        return sorted(s) == sorted(t)
 
 
 if __name__ == "__main__":
