6801b4713b93e5cd6ae8fd8924254a0f4a589c56
HerraKaava <herra.kaava@gmail.com>
2024-02-27 12:20:11 +0200

    Made the function simpler

--- LeetCode-Problems/Math/Easy/is-palindrome.py
+++ LeetCode-Problems/Math/Easy/is-palindrome.py
@@ -10,17 +10,8 @@ class Solution:
         Returns:
             bool: True if x is a palindrome, False otherwise.
         """
-        s1 = str(x)
-        s2 = ""
-        # Construct the string backwards in the loop.
-        # If x = 123, then s1 = "123" and s2 = "321".
-        for i in range(len(s1)-1, -1, -1):
-            s2 += s1[i]
-        if s1 == s2:
-            return True
-        else:
-            return False
-
+        return str(x) == str(x)[::-1]
+                
 
 if __name__ == "__main__":
     sol = Solution()
